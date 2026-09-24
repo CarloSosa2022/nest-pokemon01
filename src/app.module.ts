2c19ef439a4abe9bf2922abd392394fae244fe14
Carlos A. Cardenas <cacardenas@DGISHF26001.hacienda.ejecutivo-carabobo.gov.ve>
2026-09-24 12:26:58 -0400

    seccion 9

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,32 @@ import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration], //nos enlaza con el ConfigModule
+      validationSchema: JoiValidationSchema,
+    }), //siempre va enciam porque es quien nos permite leer las variables de entorno
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
 
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    // MongooseModule.forRoot(`${process.env.MONGODB}`, {
+    //   dbName: 'pokemonsdb',
+    // }),
+
+    //se cambió por una falla de conexión con mongo atlas
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.MONGODB,
+        dbName: 'pokemonsdb',
+      }),
+    }),
 
     PokemonModule,
 
@@ -21,4 +39,8 @@ import { SeedModule } from './seed/seed.module';
     SeedModule,
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    //console.log(process.env);
+  }
+}
